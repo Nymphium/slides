1d6d83faff36a3572a8d0d53889f0461cbef97ae
nymphium <s1311350@coins.tsuuba.ac.jp>
2016-05-13 23:43:27 +0900

    いろいろうごかした

--- tsukubapm/codes/headerprint.lua
+++
@@ -1,7 +0,0 @@
-local header = {"1B", "4C", "75", "61", "52", "00", "01", "04", "08", "04", "08", "00", "19", "93", "0D", "0A", "1A", "0A"}
-tex.print([[\only<1>{]] .. table.concat(header, " ") .."}")
-tex.print([[\only<2>{]] .. [[\alert{]] .. table.concat(header, " ", 1, 4).. [[} \textcolor{gray}{]]..table.concat(header, " ", 5, 18).. "}}")
-
-for i = 5, 12 do
-	tex.print([[\only<]].. (i - 2).. [[>{\textcolor{gray}{]] ..table.concat(header, " ", 1, i - 1) ..[[} \alert{]].. header[i].. [[} \textcolor{gray}{]].. table.concat(header, " ", i + 1, 18) .. "}}")
-end
