6a5b51faf788105702c7e2fcd3d068dcb9ad8dbe
Nymphium <s1311350@gmail.com>
2025-06-01 21:40:27 +0900

    fixup! wip fp matsuri 2025

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
 
     flake-utils.url = "github:numtide/flake-utils";
   };
